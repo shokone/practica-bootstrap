a617b541780633485a6b344b97e521a1581bab9c
shokone <imartineztutor@gmail.com>
2016-02-28 11:17:57 +0100

    creamos la pagina discografia

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,25 @@
 /**
  * funcion para la carga de imágenes en el slider
  */
-function sliderHome() {
+function slider() {
     $(function() {
         $('#slider a:gt(0)').hide();
         var tiempo = setInterval(changeDiv, 6000);
         //funcion para cambiar de una imagen a otra
         function changeDiv() {
-            $('#slider a:first-child').fadeOut(1000).next('a').fadeIn(1000).end().appendTo('#slider');
+            $('#slider a:first-child').fadeOut(1000)
+            .next('a').fadeIn(1000).end().appendTo('#slider');
         };
         //cambiar a la siguiente imagen al hacer click
-        $('.mas').click(function() {
+        $('.mas').on("click", function() {
             changeDiv();
             clearInterval(tiempo);
             tiempo = setInterval(changeDiv, 6000);
         });
         //cambiar a la anterior imagen al hacer click
-        $('.menos').click(function() {
+        $('.menos').on("click", function() {
             $('#slider a:first-child').fadeOut(1000);
-            $('#slider a:last-child').fadeIn(1000).prependTo('#slider');
+            $('#slider a:last-child').fadeIn(1000).appendTo('#slider');
             clearInterval(tiempo);
             tiempo = setInterval(changeDiv, 6000);
         });
